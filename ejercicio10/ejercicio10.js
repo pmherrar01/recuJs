6bfbadf5042409024f8e7d3fcd1f9ce170bd9c42
pmherrar01 <pmherrerar01@educarex.es>
2026-06-05 23:07:06 +0200

    ejercico 11 por teminar falta el borrar el carrito

--- ejercicio10/ejercicio10.js
+++ ejercicio10/ejercicio10.js
@@ -7,6 +7,9 @@ const errorNombre = document.getElementById("errorNombre");
 const errorDni = document.getElementById("errorDni");
 const errorCorreo = document.getElementById("errorEmail")
 const errorFecha = document.getElementById("errorFecha")
+const patronDni = /^[0-9]{8}[a-zA-Z]$/;
+const patronCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 
 function esMayorDeEdad(fechaNacimiento) {
   const hoy = new Date();
@@ -23,36 +26,54 @@ function esMayorDeEdad(fechaNacimiento) {
 
 boton.onclick = () => {
 
+    let todoCorecto = true;
 
     if(nombre.value === "" || nombre.value.length < 3){
         errorNombre.style.display = "block"
         nombre.classList.add("input-error")
+        todoCorecto = false;
+
     }else{
         errorNombre.style.display = ""
         nombre.classList.remove("input-error")
     }
 
-    if(dni.value === ""){
-        errorDni.style.display = "block";
-        dni.classList.add("input-error");
-    }else{
-        errorDni.style.display = "";
-        dni.classList.remove("input-error");
-    }
-
-    if(correo.value === ""){
-        errorCorreo.style.display = "block";
-        correo.classList.add("input-error");
-    }else{
-        errorCorreo.style.display = "";
-        correo.classList.remove("input-error");
-    }
-
     if(fecha.value === "" || !esMayorDeEdad(fecha.value)){
         errorFecha.style.display = "block";
         fecha.classList.add("input-error");
+        todoCorecto = false;
+        
     }else{
         errorFecha.style.display = "";
         fecha.classList.remove("input-error");
     }
+
+    if(dni.value === "" || !patronDni.test(dni.value)){
+
+
+        errorDni.style.display = "block";
+        dni.classList.add("input-error");
+        todoCorecto = false;
+    }else{
+        errorDni.style.display = "";
+        dni.classList.remove("input-error");
+       
+    }
+
+    if(correo.value === "" || !patronCorreo.test(correo.value)){
+        errorCorreo.style.display = "block";
+        correo.classList.add("input-error");
+        todoCorecto = false;
+    }else{
+        errorCorreo.style.display = "";
+        correo.classList.remove("input-error");
+      
+    }
+
+    let dniMayus = dni.value.toUpperCase();
+
+    if(todoCorecto){
+        window.alert(`Compra procesada con éxito. Revisando DNI: ${dniMayus}`);
+    }
+
 }
---
+++ ejercicio11/ejercicio11.js
@@ -0,0 +1,84 @@
+let catalogo = [];
+let carrito = [];
+const contenedorProductos = document.getElementById("contenedorProductos");
+const tienda = document.querySelector(".tienda");
+const carritoResumen = document.getElementById("contenedorCarrito");
+
+function pintarCatalogo(aPrendas) {
+    if (contenedorProductos) {
+        contenedorProductos.innerHTML = ""
+        aPrendas.forEach(pro => {
+            contenedorProductos.innerHTML += `
+<div class="tarjeta-producto">
+    <div class="img-container">
+        <img src="${pro.image}" alt="Imagen del producto">
+    </div>
+    <div class="info-producto">
+        <h3 class="titulo-producto">${pro.title}</h3>
+        <span class="precio-producto">$${parseFloat(pro.price).toFixed(2)}</span>
+        <button class="btn-anadir" data-id="${pro.id}">🛒 Añadir al carrito</button>
+    </div>
+</div>
+           `;
+        });
+    }
+}
+
+addEventListener("DOMContentLoaded", async () => {
+
+    fetch("https://fakestoreapi.com/products?limit=6").then((respuesta) => {
+        return respuesta.json();
+    }
+    ).then((datos) => {
+        datos.forEach(prenda => {
+            catalogo.push(prenda)
+        });
+
+        pintarCatalogo(catalogo);
+    }
+
+    )
+
+})
+
+function pintarCarrito(aCarrito) {
+    carritoResumen.innerHTML = ""
+    if (aCarrito.length <= 0) {
+        carritoResumen.innerHTML += "El carrito esta vacio";
+    } else {
+        aCarrito.forEach(proCa => {
+            carritoResumen.innerHTML += `
+            <div class="item-carrito">
+    <img src="${proCa.image}" alt="Producto" class="img-carrito">
+    <div class="info-carrito">
+        <span class="titulo-carrito">${proCa.title}</span>
+        <span class="precio-carrito">$${parseFloat(proCa.price).toFixed(2)}</span>
+    </div>
+    <button class="btn-borrar" data-id="${proCa.id}">❌</button>
+</div>
+
+            `
+        });
+    }
+}
+
+
+
+tienda.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-anadir")) {
+        let id = parseInt(event.target.dataset.id);
+        let posicion = catalogo.findIndex((pro) => pro.id === id);
+
+        carrito.push(catalogo[posicion]);
+
+        pintarCarrito(carrito);
+
+        let total = carrito.reduce((acumulador, pro) => acumulador + pro.price, 0);
+
+// ¡Faltaba inyectarlo en el DOM!
+document.getElementById("precioTotal").innerHTML = total.toFixed(2);
+
+    }
+
+
+})
